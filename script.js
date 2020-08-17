84cd07bee048d96d46c5eb135a98edc177a8149f
Arayik19 <ahambardzumyan19@gmail.com>
2020-08-17 17:00:44 +0400

    Second Commit

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 //================ Variable Declaration=========
 
-let main = document.getElementById('main')
-let c1 = 0;
-let c2 = 0;
-let c3 = 0;
+const main = document.getElementById('main');
 
 //================ Variable Declaration=========
 
@@ -13,10 +10,9 @@ for(let i = 0; i < 256; i++){
     for(let j = 0;j < 256; j++){
         let colorBlock = document.createElement('div');
         colorBlock.className = "cBlock";
-        colorBlock.style.height = "100px";
-        colorBlock.style.width = "100px";
-        colorBlock.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
-        colorBlock.style.float = 'left';            
+
+        colorBlock.style.backgroundColor = `rgb(0,${i},${j})`;
+            
         colorBlock.addEventListener("mouseenter",function(){
             colorBlock.innerHTML = `i=${i} j=${j} rgb(${0};${i};${j})`;
         })
@@ -24,10 +20,8 @@ for(let i = 0; i < 256; i++){
             colorBlock.innerHTML = "";
         })   
         main.appendChild(colorBlock);
-        c3=c3+1;
-    }    
-    c2=c2+1;
-    c3 = 0;
+        
+    } 
 }
 //==================== Matrix Creation============
 
